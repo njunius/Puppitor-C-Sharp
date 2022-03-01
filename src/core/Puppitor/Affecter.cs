9196c5205f4882dcfaf6ccf00a825ee4846b685e
Nick Junius <njunius@users.noreply.github.com>
2022-03-01 14:14:33 -0800

    real VS implementation of Puppitor
    two separate projects for Puppitor now in core
    
    Puppitor is the library itself
    
    Puppitor Test is the test functionality
    
    affect rules live in Puppitor Test for now

---
+++ src/core/Puppitor/Affecter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puppitor
+{
+    public class Affecter
+    {
+        public Affecter(double affectFloor = 0.0, double affectCeiling = 1.0, string equilibriumAction = "resting")
+        {
+
+        }
+    }
+}
